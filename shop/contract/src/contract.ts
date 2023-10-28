cd33ea2b3047cf2f0545bb7461710c5d27f718fe
seanmanik <seanmanik@gmail.com>
2023-10-28 11:55:28 -0400

    New smart contract

--- shop/contract/src/contract.ts
+++ shop/contract/src/contract.ts
@@ -3,23 +3,48 @@ import { User } from './model'
 
 @NearBindgen({})
 class GuestBook {
-  total_points: number = 0;
+  users: Vector<User> = new Vector<User>("v-uid");
 
   @call({ payableFunction: false })
-  add_points({ points }: { points: number }) {
-    const new_point = Number(this.total_points) + Number(points);
-    this.total_points = new_point;
+  add_points({ name, points }: { name:string, points: number }) {
+    const score = Number(points);
+    let person: User = { name, score };
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users.get(i)["name"] == name) {
+        const new_score = Number(this.users.get(i)["score"]) + Number(points);
+        person["score"] = Number(new_score);
+        this.users.replace(i, person);
+        return;
+      }
+    }
+    this.users.push(person);
   }
 
   @call({ payableFunction: false })
-  redeem_points({ points }: { points: number }) {
-    const point = Number(this.total_points) - Number(points);
-    this.total_points = point;
+  redeem_points({ name, points }: { name:string, points: number }) {
+
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users.get(i)["name"] == name) {
+        const score = Number(this.users.get(i)["score"]) - Number(points);
+        const person: User = { name, score };
+        this.users.replace(i, person);
+        return;
+      }
+    }
   }
 
   @view({})
-  get_points(): number { 
-    return this.total_points;
+  get_points({ user }: { user: string }): number { 
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users.get(i)["name"] === user) {
+          return this.users.get(i)["score"];
+      }
+    }  
+  }
+  
+  @view({})
+  get_all_points(): User[] {
+    return this.users.toArray();
   }
 
 }
